be277b343d497269bb6979e3e1b525f7417097c7
Jonathan Chan <johnkchan252@gmail.com>
2019-10-02 14:56:51 -0400

    Added new questions

---
+++ reverse_staircase.py
@@ -0,0 +1,11 @@
+# Function should print staircase based on argument height
+# reverse_staircase(4) would print
+#    +
+#   ++
+#  +++
+# ++++
+
+
+def reverse_staircase(height):
+    # TODO: Implement this function
+    return
---
+++ staircase.py
@@ -0,0 +1,11 @@
+# Function should print staircase based on argument height
+# staircase(4) would print
+# +
+# ++
+# +++
+# ++++
+
+
+def staircase(height):
+    # TODO: Implement this function
+    return

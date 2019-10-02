c06fafa49d6e9782cff502833d8eca0143cf74a1
Jonathan Chan <johnkchan252@gmail.com>
2019-10-02 15:18:53 -0400

    Create floors.py

---
+++ floors.py
@@ -0,0 +1,12 @@
+# Function should return floor that user ends up on if they following directions provided in argument, arr
+# User starts off on floor 0
+# + indicates to ascend one floor
+# - indicates to descen on floor
+# floors("++-+") will return floor 2
+
+directions = "+-+-++--+-++-+-+-+-++++-++----++-+"
+
+
+def floors(arr):
+   # TODO Implement this function
+    return

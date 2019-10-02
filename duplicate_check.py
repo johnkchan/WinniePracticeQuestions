690575857c314a8b2d0d5919448f78d0996c0e62
Jonathan Chan <johnkchan252@gmail.com>
2019-10-02 15:11:10 -0400

    Create duplicate_check.py

---
+++ duplicate_check.py
@@ -0,0 +1,9 @@
+# Function should return a sorted array of all duplicate elements within argument, arr
+
+
+nums = [1, 3, 4, 5, 2, 1, 5, 2, 8, 9, 0, 0]
+
+
+def duplicate_check(arr):
+   # TODO Implement this function
+    return

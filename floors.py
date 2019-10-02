32899e8c84fe7a9a2f1e827e861197d6475ce914
Jonathan Chan <johnkchan252@gmail.com>
2019-10-02 16:16:55 -0400

    Update floors.py

--- floors.py
+++ floors.py
@@ -1,7 +1,7 @@
 # Function should return floor that user ends up on if they're following directions provided in argument, arr
 # User starts off on floor 0
 # + indicates to ascend one floor
-# - indicates to descen on floor
+# - indicates to descend one floor
 # floors("++-+") will return floor 2
 
 directions = "+-+-++--+-++-+-+-+-++++-++----++-+"

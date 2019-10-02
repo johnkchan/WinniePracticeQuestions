e17002428935cd02a979acac141d5732d23da902
Jonathan Chan <johnkchan252@gmail.com>
2019-10-02 15:30:21 -0400

    Create lost_and_found.py

---
+++ lost_and_found.py
@@ -0,0 +1,9 @@
+# Function should return an array of the items you lost that were found in the lost and found pile
+
+itemsLost = {"Phone", "Flashlight", "Socks", "Sandals"}
+LostNFoundPile = {"Keys", "Headphones", "Socks", "Phone", "Watch"}
+
+
+def lost_and_found(lostItems, pile):
+   # TODO Implement this function
+    return

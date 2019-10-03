bb8f492be112ad141c81ab5dd5f6f2119796ceae
Jonathan Chan <johnkchan252@gmail.com>
2019-10-03 16:04:58 -0400

    Create prime_check.py

---
+++ prime_check.py
@@ -0,0 +1,8 @@
+# Function should return true if argument, num is prime and false if not prime
+# pyramid(5) would print
+# true
+
+
+def prime_check(num):
+   # TODO Implement this function
+    return

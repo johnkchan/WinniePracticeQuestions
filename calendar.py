6f9c912a90ac9589b02720f03fa7c3c23417217d
Jonathan Chan <johnkchan252@gmail.com>
2019-10-09 22:02:25 -0400

    Added new problems

---
+++ calendar.py
@@ -0,0 +1,13 @@
+# Function should print calendar based on arguments, startDate and dates
+# calendar("tuesday", 28) should print
+# S	M	T	W	T	F	S
+# 			   1	2	3	4
+# 5	6	7	8	9	10	11
+# 12	13	14	15	16	17	18
+# 19	20	21	22	23	24	25
+# 26	27	28
+
+
+def calendar(startDate, dates):
+   # TODO Implement this function
+    return
---
+++ min_change.py
@@ -0,0 +1,8 @@
+# Function should return the minimum amount of change needed to make argument, amnt
+# min_change(.76) returns 4 because 3 Quarters and 1 Penny is the minimum amount of change needed to make .76
+# min_change(.37) returns 4 because 1 Quarter, 1 Dime, and 2 Pennies is the minimum amount of change needed to make .37
+
+
+def min_change(amnt):
+   # TODO Implement this function
+    return

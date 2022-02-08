7a02dfb44943135111c64288e97eac04bb44b854
Jonathan Chan <16432155+jonkchan@users.noreply.github.com>
2022-02-08 17:37:05 -0500

    Update lost_and_found.py

--- lost_and_found.py
+++ lost_and_found.py
@@ -5,6 +5,6 @@ lost_and_found_pile = ["Keys", "Headphones", "Socks", "Phone",
                       "Watch", "Slippers", "Gloves", "Tie", "Bracelet"]
 
 
-def lost_and_found(lostItems: List[str], pile: List[str]) -> List[str]:
+def lost_and_found(lost_items: List[str], lost_and_found_pile: List[str]) -> List[str]:
    # TODO Implement this function
     return

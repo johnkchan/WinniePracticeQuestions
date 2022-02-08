4c64fefaf5fc7a2ecb7fd166864725cfae07bcab
Jonathan Chan <16432155+jonkchan@users.noreply.github.com>
2022-02-08 17:54:21 -0500

    Update lost_and_found.py

--- lost_and_found.py
+++ lost_and_found.py
@@ -5,6 +5,10 @@ lost_and_found_pile = ["Keys", "Headphones", "Socks", "Phone",
                       "Watch", "Slippers", "Gloves", "Tie", "Bracelet"]
 
 
-def lost_and_found(lost_items: List[str], lost_and_found_pile: List[str]) -> List[str]:
+class Solution:
+  def check_lost_and_found(self, lost_items: List[str], lost_and_found_pile: List[str]) -> List[str]:
    # TODO Implement this function
     return
+
+solution = Solution()
+print(solution.check_lost_and_found(lost_items, lost_and_found_pile))
